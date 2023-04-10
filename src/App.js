067f7e8dba96e1b376e3497ead6e14450d1eda20
faeze <faezeebold@gmail.com>
2023-04-10 13:19:28 +0330

    components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './index.css'
 
-    const user = [
-      {
-        firstname :'mina'
-      },
-      {
-        firstname : 'sina'
-      },
-      {
-        firstname : 'ali'
-      }
-    ]
 
-  return (
-    <div className="App">
-      {user.map(name => (<h1>{name.firstname}</h1>))}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <h1>شمارنده من</h1>
+    )
 }
 
 export default App;
+
