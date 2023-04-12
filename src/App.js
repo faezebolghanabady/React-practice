39dedbb6a67914b0fa47e6ad81b7ddc137059d48
faeze <faezeebold@gmail.com>
2023-04-12 20:46:12 +0330

    add component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './app.css';
+import child from './components/child';
+const App = () => {
+    return(
+        <div className="matn">
+        <h1>شمارنده من</h1>
+        <br/>
+        <child/>
+        </div>
+       
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from 'react-dom'
+import App from './App'
 
-const element = <h1>hiiiiiii</h1>
+ReactDOM.render(<App/> , document.getElementById('root'))
 
-ReactDOM.render(element , document.getElementById('root'));
+
+
+
+
