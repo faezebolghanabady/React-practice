a8dd46e36299b71dd171d987efa0cd94baea1cab
faeze <faezeebold@gmail.com>
2023-04-12 12:16:43 +0330

    add component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
+import App from "./app";
 import ReactDOM from 'react-dom'
 
-const element = <h1>hiiiiiii</h1>
 
-ReactDOM.render(element , document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
