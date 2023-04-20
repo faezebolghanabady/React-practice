612a09ea2101aa51dd0b953417af75d53c2a2785
faeze <faezeebold@gmail.com>
2023-04-20 12:46:58 +0330

    state-class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import { Component } from "react";
+
 class App extends Component{
+
+    state = {
+        family : 'bolghanabady',
+        name : 'faeze', 
+        id : 18
+
+    }
+
     render(){
+        const {family , name , id} = this.state
         return(
             <div>
                 <header>
                     <h1>شمارنده من</h1>
                 </header>
+                <p>{name}</p>
+                <p>{family}</p>
+                <p>{id}</p>
             </div>
         )
     }
 }
+
 export default App;
