32d31606c3f10839357dba29b2efd02b9c8db7a9
faeze <faezeebold@gmail.com>
2023-04-20 14:26:20 +0330

    HandingEvents

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import { useState } from "react";
 
 const App = () =>{
-    const[name , setname] = useState(initialstatte = 0);
+    const[name , setname] = useState(initialstate = 0);
     const changeName = () => {
         setname(value = 5)
     }
