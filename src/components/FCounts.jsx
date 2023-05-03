8b6b127201708dfb13df0027196e6a0334960ca4
faeze <faezeebold@gmail.com>
2023-05-03 22:25:40 +0330

    component

--- src/components/FCounts.jsx
+++ src/components/FCounts.jsx
@@ -4,7 +4,7 @@
      const FCounts = () => {
 
       const [product , productName ] = useState([
-            {id:1 , count:3 , productName:'laptop'},
+            {id:1 , count:3 , productName:'laptopp'},
             {id:2 , count:4 , productName:'phone'},
             {id:3 , count: 8, productName:'airpods'}
         ])
