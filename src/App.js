129876113ac53575f952f213c344345833dbdf51
faeze <faezeebold@gmail.com>
2023-05-06 20:29:09 +0330

    test

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { render } from "@testing-library/react";
 // import Numbers from './components/Numbers';
 // import { useState } from "react";
 // import FCounts from './components/FCounts';
-import Numbers from "./components/Numbers";
+// import Numbers from "./components/Numbers";
+import Landing from "./components/Landin";
 
 const App = () => {
 
    return(
     <div>
-      <Numbers/>
+      <Landing/>
     </div>
    )
     }
